9498c78d368628156f0cc5b7e335c5619665e1c6
TheYear2015 <TheYear2015@yeah.net>
2016-01-21 20:36:53 +0800

    android编译配置修改

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,7 +12,13 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/BaseScene.cpp \
+                   ../../Classes/GameData.cpp \
+                   ../../Classes/GameLaunchScene.cpp \
+                   ../../Classes/GameSceneManager.cpp \
+                   ../../Classes/GameWorldScene.cpp \
+                   ../../Classes/PlayGameScene.cpp \
+                   ../../Classes/GameLogic.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
